e978050058da0f5c04f3dec1c8983a24d87714a6
Matheus <matheuspatrocinio@hotmail.com>
2019-07-15 02:13:45 -0300

    spring apps generating log every 3 or 5 seconds.

--- SpringBootApps/logger3/src/main/java/br/com/example/logger3/Logger3Application.java
+++ SpringBootApps/logger3/src/main/java/br/com/example/logger3/Logger3Application.java
@@ -1,13 +1,43 @@
 package br.com.example.logger3;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
+
 public class Logger3Application {
+	private static final Logger logger = LoggerFactory.getLogger(Logger3Application.class);
+	private static final int TIME=3;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Logger3Application.class, args);
+		generateLog();
+	}
+
+	public static void generateLog() {
+		long timestamp;
+		while (true) {
+			timestamp = System.currentTimeMillis() / 1000;
+			if (timestamp % TIME == 0) {
+				logger.debug("debug");
+				logger.info("info");
+				logger.warn("warning");
+				logger.error("error");
+				
+				try {
+					Thread.sleep((long) (Math.random()*1000));
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 }
--- SpringBootApps/logger5/src/main/java/br/com/example/logger5/Logger5Application.java
+++ SpringBootApps/logger5/src/main/java/br/com/example/logger5/Logger5Application.java
@@ -1,13 +1,41 @@
 package br.com.example.logger5;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Logger5Application {
-
+	
+	private static final Logger logger = LoggerFactory.getLogger(Logger5Application.class);
+	private static final int TIME=5;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Logger5Application.class, args);
+		generateLog();
+	}
+
+
+	public static void generateLog() {
+		long timestamp;
+		while (true) {
+			timestamp = System.currentTimeMillis() / 1000;
+			if (timestamp % TIME == 0) {
+				logger.debug("DEBUG");
+				logger.info("INFO");
+				logger.warn("WARNING");
+				logger.error("ERROR");
+				
+				try {
+					Thread.sleep((long) (Math.random()*1000));
+				} catch (InterruptedException e) {
+					System.out.println(e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
 }
